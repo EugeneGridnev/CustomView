fe7745e2dfac00dd268d1559c9e2de1275d7beb6
EugeneGridnev <eugenegrid@yandex.ru>
2024-02-27 21:03:37 +0300

    onSizeChanged and onMeasure added

--- app/src/main/java/com/eugeneprojects/customview/TicTacToeView.kt
+++ app/src/main/java/com/eugeneprojects/customview/TicTacToeView.kt
@@ -2,9 +2,12 @@ package com.eugeneprojects.customview
 
 import android.content.Context
 import android.graphics.Color
+import android.graphics.RectF
 import android.util.AttributeSet
+import android.util.TypedValue
 import android.view.View
-import android.widget.GridView
+import kotlin.math.max
+import kotlin.math.min
 import kotlin.properties.Delegates
 
 
@@ -20,6 +23,7 @@ class TicTacToeView(
             field?.liseners?.remove(listener)
             field = value
             field?.liseners?.add(listener)
+            updateViewSize()
             requestLayout()
             invalidate()
         }
@@ -27,6 +31,10 @@ class TicTacToeView(
     private var player1Color by Delegates.notNull<Int>()
     private var player2Color by Delegates.notNull<Int>()
     private var gridColor by Delegates.notNull<Int>()
+
+    private val fieldRect = RectF(0f,0f,0f,0f)
+    private var cellSize: Float = 0f
+    private var cellPadding: Float = 0f
 
     constructor(context: Context, attributesSet: AttributeSet?, defStyleAttr: Int) : this(context, attributesSet, defStyleAttr, R.style.DefaultTicTacToeFieldStyle)
     constructor(context: Context, attributesSet: AttributeSet?) : this(context, attributesSet, R.attr.ticTacToeFieldStyle)
@@ -48,6 +56,52 @@ class TicTacToeView(
     override fun onDetachedFromWindow() {
         super.onDetachedFromWindow()
         ticTacToeField?.liseners?.remove(listener)
+    }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        val minWidth = suggestedMinimumWidth + paddingLeft + paddingRight
+        val minHeight = suggestedMinimumHeight + paddingTop + paddingBottom
+
+        val desiredCellSizeInPixels = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,
+            DESIRED_CELL_SIZE.toFloat(),
+            resources.displayMetrics).toInt()
+        val rows = ticTacToeField?.rows ?: 0
+        val columns = ticTacToeField?.columns ?: 0
+
+        val desiredWidth = max(minWidth, columns * desiredCellSizeInPixels + paddingLeft + paddingRight)
+        val desiredHeight = max(minHeight, rows * desiredCellSizeInPixels + paddingTop + paddingBottom)
+
+        setMeasuredDimension(
+            resolveSize(desiredWidth, widthMeasureSpec),
+            resolveSize(desiredHeight, heightMeasureSpec)
+        )
+    }
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+        updateViewSize()
+
+    }
+
+    private fun updateViewSize() {
+        val field = this.ticTacToeField ?: return
+
+        val safeWidth = width - paddingLeft - paddingRight
+        val safeHeight = height - paddingTop - paddingBottom
+
+        val cellWidth = safeWidth / field.columns.toFloat()
+        val cellHeight = safeHeight / field.rows.toFloat()
+
+        cellSize = min(cellWidth, cellHeight)
+        cellPadding = cellSize * 0.2f
+
+        val fieldWidth = cellSize * field.columns
+        val fieldHeight = cellSize * field.rows
+
+        fieldRect.left = paddingLeft + (safeWidth - fieldWidth) / 2
+        fieldRect.top = paddingTop + (safeHeight - fieldHeight) / 2
+        fieldRect.right = fieldRect.left + fieldWidth
+        fieldRect.bottom = fieldRect.top + fieldHeight
     }
 
     private fun initAttributes(attributesSet: AttributeSet?, defStyleAttr: Int, defStyleRes: Int) {
@@ -73,5 +127,7 @@ class TicTacToeView(
         const val PLAYER1_DEFAULT_COLOR = Color.GREEN
         const val PLAYER2_DEFAULT_COLOR = Color.RED
         const val GRID_DEFAULT_COLOR = Color.GRAY
+
+        const val DESIRED_CELL_SIZE = 50
     }
 }
