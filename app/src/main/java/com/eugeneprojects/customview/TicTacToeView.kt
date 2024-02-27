f94f0c76183ec02bad2cbd95e45bccd5f9f431a2
EugeneGridnev <eugenegrid@yandex.ru>
2024-02-27 20:16:56 +0300

    view attrs and initial logic added

--- app/src/main/java/com/eugeneprojects/customview/TicTacToeView.kt
+++ app/src/main/java/com/eugeneprojects/customview/TicTacToeView.kt
@@ -1,8 +1,11 @@
 package com.eugeneprojects.customview
 
 import android.content.Context
+import android.graphics.Color
 import android.util.AttributeSet
 import android.view.View
+import android.widget.GridView
+import kotlin.properties.Delegates
 
 
 class TicTacToeView(
@@ -11,17 +14,41 @@ class TicTacToeView(
     defStyleAttr: Int,
     defStyleRes: Int
 ) : View(context, attributesSet, defStyleAttr, defStyleRes) {
-    constructor(context: Context, attributesSet: AttributeSet?, defStyleAttr: Int) : this(context, attributesSet, defStyleAttr, 0)
-    constructor(context: Context, attributesSet: AttributeSet?) : this(context, attributesSet, 0)
+
+    private var player1Color by Delegates.notNull<Int>()
+    private var player2Color by Delegates.notNull<Int>()
+    private var gridColor by Delegates.notNull<Int>()
+
+    constructor(context: Context, attributesSet: AttributeSet?, defStyleAttr: Int) : this(context, attributesSet, defStyleAttr, R.style.DefaultTicTacToeFieldStyle)
+    constructor(context: Context, attributesSet: AttributeSet?) : this(context, attributesSet, R.attr.ticTacToeFieldStyle)
     constructor(context: Context) : this(context, null)
 
     init {
         if (attributesSet != null) {
             initAttributes(attributesSet, defStyleAttr, defStyleRes)
+        } else {
+            initDefaultColor()
         }
     }
 
     private fun initAttributes(attributesSet: AttributeSet?, defStyleAttr: Int, defStyleRes: Int) {
+        val typedArray = context.obtainStyledAttributes(attributesSet, R.styleable.TicTacToeView, defStyleAttr, defStyleRes)
+        player1Color = typedArray.getColor(R.styleable.TicTacToeView_player1Color, PLAYER1_DEFAULT_COLOR)
+        player2Color = typedArray.getColor(R.styleable.TicTacToeView_player2Color, PLAYER2_DEFAULT_COLOR)
+        gridColor = typedArray.getColor(R.styleable.TicTacToeView_gridColor, GRID_DEFAULT_COLOR)
 
+        typedArray.recycle()
+    }
+
+    private fun initDefaultColor() {
+        player1Color = PLAYER1_DEFAULT_COLOR
+        player2Color = PLAYER2_DEFAULT_COLOR
+        gridColor = GRID_DEFAULT_COLOR
+    }
+
+    companion object {
+        const val PLAYER1_DEFAULT_COLOR = Color.GREEN
+        const val PLAYER2_DEFAULT_COLOR = Color.RED
+        const val GRID_DEFAULT_COLOR = Color.GRAY
     }
 }
